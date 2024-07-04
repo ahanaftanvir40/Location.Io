93d380a8d91c0afee728eda2d47c1fe32daa4a49
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-07-05 05:34:34 +0600

    .

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ dotenv.config()
 const app = express()
 
 app.set('view engine', 'ejs')
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
