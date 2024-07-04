7d93e8c85c917fdb0c1f08f27d88894aba0fda49
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-07-05 05:50:47 +0600

    .

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { Socket, Server } from 'socket.io'
 import http from 'http'
 import dotenv from 'dotenv'
+import cors from cors
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
@@ -13,6 +14,7 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', join(__dirname, 'views'));
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(join(__dirname, 'public')))
